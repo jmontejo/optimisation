ed5fac6949f69fd283c6595b485f44730d57e7b3
Daniela <boerner@physik.uni-wuppertal.de>
2015-05-12 16:07:51 +0200

    the class definition for the python configuration file

---
+++ configuration.py
@@ -0,0 +1,11 @@
+class Configuration(object):
+	def __init__(self):
+		self.sigTree = None
+		self.bkgTreeList = None
+		self.Variables = None
+		self.preselection = "1"
+		self.event_weight = 1.
+		self.enable_plots = False
+		self.rankingMethod = "sig"
+		self.optimisationMethod = "sig"
+		self.method = None
